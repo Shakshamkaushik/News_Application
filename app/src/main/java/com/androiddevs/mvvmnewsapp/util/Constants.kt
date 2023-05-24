10cc52994ca76054be4fc9d8f3e6360e61e4f4be
ShakshamKaushik <shakshamkaushik1@gmail.com>
2023-05-24 15:26:58 +0530

    News Application

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -0,0 +1,10 @@
+package com.androiddevs.mvvmnewsapp.util
+
+class Constants {
+    companion object {
+        const val API_KEY = "c002f27868dd4bbdaf4b5532bdbc5cec"
+        const val BASE_URL = "https://newsapi.org"
+        const val SEARCH_NEWS_TIME_DELAY = 500L
+        const val QUERY_PAGE_SIZE = 20
+    }
+}
